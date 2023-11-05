2541a935db774b588d097ca5b054927f17c7fd84
Nemeziz10 <parikhviral02@gmail.com>
2023-11-05 14:34:31 +0530

    Small bugs fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Categories from './components/Categories';
-import Celebrating from './components/Celebrating';
-import Formfooter from './components/Formfooter';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
+// import Categories from './components/Categories';
+// import Celebrating from './components/Celebrating';
+// import Formfooter from './components/Formfooter';
 import Homepage from './components/Homepage';
-import Journey from './components/Journey';
-import Lucky from './components/Lucky';
+// import Journey from './components/Journey';
+// import Lucky from './components/Lucky';
 import Winners from './components/Winners';
 
 function App() {

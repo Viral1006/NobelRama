0648bf9828543bb2848ab8b799acb5a177e99520
Nemeziz10 <parikhviral02@gmail.com>
2023-11-05 14:41:03 +0530

    Small bugs fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // import Categories from './components/Categories';
 // import Celebrating from './components/Celebrating';
 // import Formfooter from './components/Formfooter';
@@ -11,12 +11,12 @@ import Winners from './components/Winners';
 function App() {
   return (
     <div className="App">
-     <Router>
-        <Routes>
+     <Routes>
+        
           <Route path="/" element={<Homepage />} />
           <Route path="/winners" element={<Winners />} />
+       
         </Routes>
-        </Router>
       
     </div>
   );

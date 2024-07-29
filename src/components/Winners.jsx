0733a60dea115df77994e6976fc40b2d1299c2bc
Nemeziz10 <parikhviral02@gmail.com>
2024-07-29 20:52:19 +0530

    Winners details modal  added

--- src/components/Winners.jsx
+++ src/components/Winners.jsx
@@ -102,8 +102,207 @@
 
 // export default Winners;
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const Winners = () => {
+//   const [nobelPrizes, setNobelPrizes] = useState([]);
+//   const [filteredPrizes, setFilteredPrizes] = useState([]);
+//   const [selectedCategory, setSelectedCategory] = useState('');
+//   const [selectedYear, setSelectedYear] = useState('');
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     axios.get('https://api.nobelprize.org/v1/prize.json')
+//       .then(response => {
+//         console.log('API Response:', response.data); // Log the API response
+//         setNobelPrizes(response.data.prizes);
+//         setFilteredPrizes(response.data.prizes);
+//         setLoading(false);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching data:', error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   const handleCategoryClick = (category) => {
+//     const filtered = nobelPrizes.filter(prize => 
+//       prize.category === category && (!selectedYear || prize.year === selectedYear)
+//     );
+//     setFilteredPrizes(filtered);
+//     setSelectedCategory(category);
+//   };
+
+//   const handleYearChange = (year) => {
+//     const filtered = nobelPrizes.filter(prize => 
+//       (!selectedCategory || prize.category === selectedCategory) && (!year || prize.year === year)
+//     );
+//     setFilteredPrizes(filtered);
+//     setSelectedYear(year);
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>; // or any loading indicator you prefer
+//   }
+
+//   const currentYear = new Date().getFullYear();
+//   const startYear = 1901;
+
+//   return (
+//     <div className='bg-[#ffffff] overflow-visible w-full h-auto min-h-[850px] flex flex-col items-center justify-center'>
+//       <div className='font-abril text-[40px] mt-8 mb-10 text-[#333333]'>
+//         Nobel Winners
+//       </div>
+
+//       <div className='flex items-center space-x-4 mb-4 mr-[80%]'>
+//         <label htmlFor='yearFilter' className='font-zilla-slab text-lg'>Filter by Year:</label>
+//         <select
+//           id='yearFilter'
+//           className='w-32 h-10 border border-gray-400 rounded-lg p-2 focus:outline-none'
+//           value={selectedYear}
+//           onChange={(e) => handleYearChange(e.target.value)}
+//         >
+//           <option value=''>All</option>
+//           {Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i).map(year => (
+//             <option key={year} value={year}>
+//               {year}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+
+//       <div className='grid grid-cols-4 grid-rows-4 gap-4 font-abril mx-4'>
+//         {filteredPrizes.map((prize, index) => (
+//           <div key={index} className='w-auto h-48 px-20 py-32 bg-[#ebebeb] text-[#333333] rounded-xl'>
+//             <div className='text-lg font-abril -mt-16'>{prize.year}</div>
+//             <div className='font-zilla-slab text-base mt-2'>{prize.category}</div>
+//             {prize.laureates && prize.laureates.map((laureate, innerIndex) => (
+//               <div key={`${index}-${innerIndex}`} className='font-abril text-base mt-2'>
+//                 {laureate.firstname} {laureate.surname}
+//               </div>
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Winners;
+
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+// import logo1 from '/logo2.png';
+
+// const Winners = () => {
+//   const [nobelPrizes, setNobelPrizes] = useState([]);
+//   const [filteredPrizes, setFilteredPrizes] = useState([]);
+//   const [selectedCategory, setSelectedCategory] = useState('');
+//   const [selectedYear, setSelectedYear] = useState('');
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     axios.get('https://api.nobelprize.org/v1/prize.json')
+//       .then(response => {
+//         console.log('API Response:', response.data); 
+//         setNobelPrizes(response.data.prizes);
+//         setFilteredPrizes(response.data.prizes);
+//         setLoading(false);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching data:', error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   const handleCategoryClick = (category) => {
+//     const filtered = nobelPrizes.filter(prize => 
+//       prize.category === category && (!selectedYear || prize.year === selectedYear)
+//     );
+//     setFilteredPrizes(filtered);
+//     setSelectedCategory(category);
+//   };
+
+//   const handleYearChange = (year) => {
+//     const filtered = nobelPrizes.filter(prize => 
+//       (!selectedCategory || prize.category === selectedCategory) && (!year || prize.year === year)
+//     );
+//     setFilteredPrizes(filtered);
+//     setSelectedYear(year);
+//   };
+
+//   if (loading) {
+//     return <div>
+//       <div id="loader" className="fixed inset-0 flex items-center justify-center bg-[#cacaca] z-50">
+//     <div className="flex items-center">
+//       <h2 className="text-2xl text-[#343434] font-semibold mr-2">Hang on,</h2>
+//       <h2 className="text-2xl text-[#343434] font-semibold mr-2">Looking for new winners</h2>
+//       <div className="flex dot-animation">
+//         <div className="dot dot-1"></div>
+//         <div className="dot dot-2"></div>
+//         <div className="dot dot-3"></div>
+//       </div>
+//     </div>
+//   </div>
+//   </div>; // or any loading indicator you prefer
+//   }
+
+//   const currentYear = new Date().getFullYear();
+//   const startYear = 1901;
+
+//   return (
+//     <div className=' bg-[#F5F3F5] overflow-visible w-full h-auto min-h-[850px] flex flex-col items-center justify-center'>
+//       <div className='w-full flex items-center justify-between p-4'>
+//         <Link to='/' className='flex items-center'>
+//           <img src={logo1} alt='Logo' className='w-14 ml-2 rounded-[50%] h-auto' />
+//         </Link>
+//       </div>
+//       <div className='font-abril text-[40px] -mt-16 mb-10 text-[#333333]'>
+//           Nobel Winners
+//       </div>
+
+//       <div className='flex items-center space-x-4 mb-4 mr-[80%]'>
+//         <label htmlFor='yearFilter' className='font-zilla-slab text-lg'>Filter by Year:</label>
+//         <select
+//           id='yearFilter'
+//           className='w-32 h-10 border-none border-gray-400 bg-[#dcdcdc] text-[#333333] rounded-lg p-2 focus:outline-none'
+//           value={selectedYear}
+//           onChange={(e) => handleYearChange(e.target.value)}
+//         >
+//           <option value=''>All</option>
+//           {Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i).map(year => (
+//             <option key={year} value={year}>
+//               {year}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+
+//       <div className='grid grid-cols-4 grid-rows-4 gap-4 font-abril  mx-4'>
+//         {filteredPrizes.map((prize, index) => (
+//           <div key={index} className='flex flex-col items-center justify-center shadow-lg w-auto h-48 px-20 py-32 bg-[#dcdcdc] text-[#333333] rounded-xl'>
+//             <div className='text-lg font-abril -mt-16'>{prize.year}</div>
+//             <div className='font-zilla-slab text-base mt-2'>{prize.category}</div>
+//             {prize.laureates && prize.laureates.map((laureate, innerIndex) => (
+//               <div key={`${index}-${innerIndex}`} className='font-abril text-base mt-2'>
+//                 {laureate.firstname} {laureate.surname}
+//               </div>
+//             ))}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Winners;
+
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logo1 from '/logo2.png';
 
 const Winners = () => {
   const [nobelPrizes, setNobelPrizes] = useState([]);
@@ -111,11 +310,12 @@ const Winners = () => {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedYear, setSelectedYear] = useState('');
   const [loading, setLoading] = useState(true);
+  const [selectedWinner, setSelectedWinner] = useState(null); // State for selected winner
 
   useEffect(() => {
     axios.get('https://api.nobelprize.org/v1/prize.json')
       .then(response => {
-        console.log('API Response:', response.data); // Log the API response
+        console.log('API Response:', response.data); 
         setNobelPrizes(response.data.prizes);
         setFilteredPrizes(response.data.prizes);
         setLoading(false);
@@ -142,24 +342,37 @@ const Winners = () => {
     setSelectedYear(year);
   };
 
+  const handleBoxClick = (prize) => {
+    setSelectedWinner(prize); // Set selected winner's details
+  };
+
+  const handleCloseModal = () => {
+    setSelectedWinner(null); // Close the modal
+  };
+
   if (loading) {
-    return <div>Loading...</div>; // or any loading indicator you prefer
+    return <div>Loading...</div>;
   }
 
   const currentYear = new Date().getFullYear();
   const startYear = 1901;
 
   return (
-    <div className='bg-[#ffffff] overflow-visible w-full h-auto min-h-[850px] flex flex-col items-center justify-center'>
-      <div className='font-abril text-[40px] mt-8 mb-10 text-[#333333]'>
-        Nobel Winners
+    <div className='bg-[#F5F3F5] overflow-visible w-full h-auto min-h-[850px] flex flex-col items-center justify-center'>
+      <div className='w-full flex items-center justify-between p-4'>
+        <Link to='/' className='flex items-center'>
+          <img src={logo1} alt='Logo' className='w-14 ml-2 rounded-[50%] h-auto' />
+        </Link>
+      </div>
+      <div className='font-abril text-[40px] -mt-16 mb-10 text-[#333333]'>
+          Nobel Winners
       </div>
 
       <div className='flex items-center space-x-4 mb-4 mr-[80%]'>
         <label htmlFor='yearFilter' className='font-zilla-slab text-lg'>Filter by Year:</label>
         <select
           id='yearFilter'
-          className='w-32 h-10 border border-gray-400 rounded-lg p-2 focus:outline-none'
+          className='w-32 h-10 border-none border-gray-400 bg-[#dcdcdc] text-[#333333] rounded-lg p-2 focus:outline-none'
           value={selectedYear}
           onChange={(e) => handleYearChange(e.target.value)}
         >
@@ -174,7 +387,11 @@ const Winners = () => {
 
       <div className='grid grid-cols-4 grid-rows-4 gap-4 font-abril mx-4'>
         {filteredPrizes.map((prize, index) => (
-          <div key={index} className='w-auto h-48 px-20 py-32 bg-[#ebebeb] text-[#333333] rounded-xl'>
+          <div
+            key={index}
+            className='flex flex-col items-center justify-center shadow-lg w-auto h-48 px-20 py-32 bg-[#dcdcdc] text-[#333333] rounded-xl cursor-pointer'
+            onClick={() => handleBoxClick(prize)}
+          >
             <div className='text-lg font-abril -mt-16'>{prize.year}</div>
             <div className='font-zilla-slab text-base mt-2'>{prize.category}</div>
             {prize.laureates && prize.laureates.map((laureate, innerIndex) => (
@@ -185,6 +402,28 @@ const Winners = () => {
           </div>
         ))}
       </div>
+
+      {/* Modal for displaying winner details */}
+      {selectedWinner && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+          <div className='flex flex-col items-center justify-center shadow-lg w-auto h-auto px-20 py-16 bg-[#dcdcdc] text-[#333333] rounded-xl'>
+            <h2 className='text-xl font-abril mb-4'>Winner Details</h2>
+            {selectedWinner.laureates.map(laureate => (
+              <div key={laureate.id} className='mb-4'>
+                <h3 className='text-lg font-abril mb-1'>{laureate.firstname} {laureate.surname}</h3>
+                <p><strong>Motivation:</strong> {laureate.motivation}</p>
+                <p className='font-zilla mb-2'><strong>Share:</strong> {laureate.share}</p>
+              </div>
+            ))}
+            <button
+              className='mt-4 bg-blue-500 text-white px-4 py-2 rounded'
+              onClick={handleCloseModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
